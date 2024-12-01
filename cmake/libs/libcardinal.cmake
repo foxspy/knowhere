a05eeb21c62c6b7a6e850c89e988d4e65d4c22db
foxspy <xian_hust@foxmail.com>
2024-12-01 17:31:11 +0800

    add log
    Signed-off-by: foxspy <xian_hust@foxmail.com>

--- cmake/libs/libcardinal.cmake
+++ cmake/libs/libcardinal.cmake
@@ -1,6 +1,6 @@
 # Use short SHA1 as version
-set(CARDINAL_VERSION v2.4.13 )
-set(CARDINAL_REPO_URL "https://github.com/zilliztech/cardinal.git")
+set(CARDINAL_VERSION segvfix_code_1 )
+set(CARDINAL_REPO_URL "https://github.com/foxspy/cardinal.git")
 
 set(CARDINAL_REPO_DIR "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cardinal")
 
--- cmake/utils/compile_flags.cmake
+++ cmake/utils/compile_flags.cmake
@@ -22,9 +22,9 @@ if(__X86_64)
 endif()
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fstack-protector -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG")
 
 if(WITH_RAFT)
   set(CMAKE_CUDA_FLAGS_DEBUG "-O0 -g -Xcompiler=-w ")
-  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -fstack-protector -DNDEBUG -Xcompiler=-w")
+  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG -Xcompiler=-w")
 endif()
