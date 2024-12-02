d1b15ad47f226ebbbc84bd65f690f311e911b53e
xianliang.li <xianliang.li@zilliz.com>
2024-12-02 18:16:25 +0800

    code v3
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/utils/compile_flags.cmake
+++ cmake/utils/compile_flags.cmake
@@ -26,5 +26,5 @@ set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG")
 
 if(WITH_RAFT)
   set(CMAKE_CUDA_FLAGS_DEBUG "-O0 -g -Xcompiler=-w ")
-  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG -Xcompiler=-w")
+  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -DNDEBUG -Xcompiler=-w")
 endif()
