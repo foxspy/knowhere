f22f7b93eeb4cbae9a7bbd5141a6bc2609f29739
xianliang.li <xianliang.li@zilliz.com>
2024-12-03 16:46:35 +0800

    add frame protect
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/utils/compile_flags.cmake
+++ cmake/utils/compile_flags.cmake
@@ -22,9 +22,9 @@ if(__X86_64)
 endif()
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
-set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -fstack-protector-all -DNDEBUG")
 
 if(WITH_RAFT)
   set(CMAKE_CUDA_FLAGS_DEBUG "-O0 -g -Xcompiler=-w ")
-  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -fno-omit-frame-pointer -DNDEBUG -Xcompiler=-w")
+  set(CMAKE_CUDA_FLAGS_RELEASE "-O3 -fstack-protector-all -DNDEBUG -Xcompiler=-w")
 endif()
