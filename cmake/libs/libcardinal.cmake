96e71fe0c3f0df0e4be6f1e4a13952b3c0c7e16d
xianliang.li <xianliang.li@zilliz.com>
2024-12-04 12:06:14 +0800

    add npe
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/libs/libcardinal.cmake
+++ cmake/libs/libcardinal.cmake
@@ -1,5 +1,5 @@
 # Use short SHA1 as version
-set(CARDINAL_VERSION find_segv_2414_435 )
+set(CARDINAL_VERSION find_segv_2414_435_npe )
 set(CARDINAL_REPO_URL "https://github.com/zilliztech/cardinal.git")
 
 set(CARDINAL_REPO_DIR "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cardinal")
