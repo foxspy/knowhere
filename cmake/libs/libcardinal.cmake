f18c61850d248a65e599140a61add4ce427bc40a
xianliang.li <xianliang.li@zilliz.com>
2025-04-23 01:02:08 +0800

    rabitq e2e test
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/libs/libcardinal.cmake
+++ cmake/libs/libcardinal.cmake
@@ -1,6 +1,6 @@
 # Use short SHA1 as version
 set(CARDINAL_VERSION rabit_e2e_test )
-set(CARDINAL_REPO_URL "https://github.com/foxspy/cardinal.git")
+set(CARDINAL_REPO_URL "https://github.com/zilliztech/cardinal.git")
 
 set(CARDINAL_REPO_DIR "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/cardinal")
 
