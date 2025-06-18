34ee335232dc021ec3db6b3f400963f0e20968fc
xianliang.li <xianliang.li@zilliz.com>
2025-06-18 17:39:14 +0800

    enable mmap test
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- tests/ut/test_search.cc
+++ tests/ut/test_search.cc
@@ -190,8 +190,7 @@ TEST_CASE("Test Mem Index With Float Vector", "[float metrics]") {
 
             REQUIRE(idx.Serialize(bs) == knowhere::Status::success);
         }
-        // search process
-        auto load_with_mmap = GENERATE(as<bool>{}, false);
+        auto load_with_mmap = GENERATE(as<bool>{}, false, true);
         {
             auto idx_expected = knowhere::IndexFactory::Instance().Create<knowhere::fp32>(name, version);
             auto idx = idx_expected.value();
