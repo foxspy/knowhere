2203f5a69b39e0deac948505089f680f2b647037
xianliang.li <xianliang.li@zilliz.com>
2025-09-11 09:04:55 +0800

    update flgs
    Signed-off-by: xianliang.li <xianliang.li@zilliz.com>

--- cmake/utils/compile_flags.cmake
+++ cmake/utils/compile_flags.cmake
@@ -23,7 +23,7 @@ if(__X86_64)
 endif()
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
-set(CMAKE_CXX_FLAGS_RELEASE "-Og -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELEASE "-O3 -g -DNDEBUG")
 
 if(WITH_CUVS)
   set(CMAKE_CUDA_FLAGS_DEBUG "-O0 -g -Xcompiler=-w ")
